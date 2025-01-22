3381d1af99e222d5f386fcd5bdc031280e2f02b3
Isa Tarmana <isatarmanamustopa54@gmail.com>
2025-01-22 22:11:49 +0700

    fixed

--- new-ikea/tailwind.config.ts
+++ new-ikea/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,18 +11,12 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#FF0000",
+        secondary: "#00FF00",
       },
     },
   },
-  module.exports = {
-    theme : {
-      extend:{
-        colors:{
-          primary: "#FF0000",
-          secondary: "#00FF00"
-        }
-      }
-    }
-  },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
